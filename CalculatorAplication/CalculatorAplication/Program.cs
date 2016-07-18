41ee50442eb861c0dee7b2b71f5e57a362254775
Cristian Zatt <cristian.zatt@gmail.com>
2016-07-18 16:56:35 -0300

    calculator

---
+++ CalculatorAplication/CalculatorAplication/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorAplication
+{
+    class NumberManipulator
+    {
+        static void Main(string[] args)
+        {
+            int a = 100;
+            int b = 200;
+            int ret;
+            NumberManipulator n = new NumberManipulator();
+
+            ret = n.FindMax(a, b);
+
+            Console.WriteLine("Max Value is: {0}", ret);
+            Console.ReadLine();
+        }
+        public int FindMax(int num1, int num2)
+        {
+            int result;
+
+            if (num1 > num1){
+                result =  num1;
+            }
+            else{
+                result = num2;
+            }
+            return result;
+        }
+
+    }
+}
